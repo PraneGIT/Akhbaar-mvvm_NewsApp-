6396587fb14b5c07338106a45166f1020b756320
praneki <pranjaljha00@gmail.com>
2023-01-30 18:51:00 +0530

    room Database and converter implemented

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/db/ArticleDao.kt
@@ -0,0 +1,17 @@
+package com.androiddevs.mvvmnewsapp.ui.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.androiddevs.mvvmnewsapp.ui.models.Article
+
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUpdate(article: Article):Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles():LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
